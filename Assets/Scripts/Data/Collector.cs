c8e41a4c905043df54df3430c6d2d87e27273731
Erickson <eagramonte@intellisys.com.do>
2023-07-13 08:40:32 -0400

    first commit

---
+++ Assets/Scripts/Data/Collector.cs
@@ -0,0 +1,9 @@
+
+using UnityEngine;
+
+public struct Collector
+{
+    public Transform point;
+    public float amount;
+    public bool IsUnderground;
+}
---
+++ Assets/Scripts/Shovel.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shovel : MonoBehaviour
+{
+    public Transform edgeRoot;
+    public LayerMask groundMask;
+
+    List<Collector> collectors = new List<Collector>();
+    Transform[] edgePoints;
+    float[] sandAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        edgePoints = new Transform[edgeRoot.transform.childCount];
+        sandAmount = new float[edgePoints.Length];
+        for (int i = 0; i < edgeRoot.transform.childCount; i++)
+        {
+            // edgePoints[i] = edgeRoot.GetChild(i);
+            var c = new Collector();
+            c.point = edgeRoot.GetChild(i);
+            c.amount = 0f;
+            c.IsUnderground = false;
+            collectors.Add(c);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < collectors.Count; i++)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(collectors[i].point.position, Vector3.up, out hit, 1, groundMask))
+            {
+                Debug.DrawLine(collectors[i].point.position, Vector3.up * hit.distance, Color.red);
+                if (hit.distance > sandAmount[i])
+                {
+                    sandAmount[i] = hit.distance;
+                }
+            }
+            else
+            {
+
+                Debug.DrawRay(edgePoints[i].position, Vector3.up, Color.green);
+            }
+        }
+    }
+}
