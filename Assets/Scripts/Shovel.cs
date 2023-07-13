6ea4804233c84e4d15ed4d77700eed44c7e55a7f
Erickson <eagramonte@intellisys.com.do>
2023-07-13 08:56:46 -0400

    44:43 added complete Collector struct

--- Assets/Scripts/Shovel.cs
+++ Assets/Scripts/Shovel.cs
@@ -8,16 +8,11 @@ public class Shovel : MonoBehaviour
     public LayerMask groundMask;
 
     List<Collector> collectors = new List<Collector>();
-    Transform[] edgePoints;
-    float[] sandAmount;
     // Start is called before the first frame update
     void Start()
     {
-        edgePoints = new Transform[edgeRoot.transform.childCount];
-        sandAmount = new float[edgePoints.Length];
         for (int i = 0; i < edgeRoot.transform.childCount; i++)
         {
-            // edgePoints[i] = edgeRoot.GetChild(i);
             var c = new Collector();
             c.point = edgeRoot.GetChild(i);
             c.amount = 0f;
@@ -29,22 +24,23 @@ public class Shovel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < edgePoints.Length; i++)
+        for (int i = 0; i < collectors.Count; i++)
         {
+            var s = collectors[i];
             RaycastHit hit;
-            if (Physics.Raycast(edgePoints[i].position, Vector3.up, out hit, 1, groundMask))
+            if (Physics.Raycast(s.point.position, Vector3.up, out hit, 1, groundMask))
             {
-                Debug.Log(hit.distance);
-                Debug.DrawLine(edgePoints[i].position, Vector3.up * hit.distance, Color.red);
-                if (hit.distance > sandAmount[i])
+                s.IsUnderground = true;
+                if (hit.distance > s.amount)
                 {
-                    sandAmount[i] = hit.distance;
+                    s.amount = hit.distance;
                 }
+                Debug.DrawLine(s.point.position, Vector3.up * hit.distance, Color.red);
             }
             else
             {
-
-                Debug.DrawRay(edgePoints[i].position, Vector3.up, Color.green);
+                s.IsUnderground = false;
+                Debug.DrawRay(s.point.position, Vector3.up, Color.green);
             }
         }
     }
