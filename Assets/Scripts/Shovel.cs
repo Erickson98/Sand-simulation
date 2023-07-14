43206752ad1786f3316537ec43a3bb525d8aa1b8
Erickson <eagramonte@intellisys.com.do>
2023-07-14 05:17:04 -0400

    redoing

--- Assets/Scripts/Shovel.cs
+++ Assets/Scripts/Shovel.cs
@@ -1,83 +1,143 @@
-using System.Collections;
-using System.Collections.Generic;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Shovel : MonoBehaviour
+// {
+//     public Transform edgeRoot;
+//     public LayerMask groundMask;
+//     // public MeshFilter sandMesh;
+//     List<Collector> collectors = new List<Collector>();
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         for (int i = 0; i < edgeRoot.transform.childCount; i++)
+//         {
+//             var child = edgeRoot.GetChild(i);
+//             if (child.gameObject.activeSelf == false)
+//             {
+//                 continue;
+//             }
+//             var c = new Collector();
+//             c.point = child;
+//             c.amount = Vector3.zero;
+//             c.IsUnderground = false;
+//             collectors.Add(c);
+//         }
+//         var mesh = new Mesh();
+//         // sandMesh.mesh = mesh;
+
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         for (int i = 0; i < collectors.Count; i++)
+//         {
+//             var s = collectors[i];
+//             RaycastHit hit;
+//             if (Physics.Raycast(s.point.position, Vector3.up, out hit, 1, groundMask))
+//             {
+//                 s.IsUnderground = true;
+//                 if (hit.distance > s.amount.y)
+//                 {
+//                     s.amount.y = hit.distance;
+//                 }
+//                 Debug.DrawLine(s.point.position, Vector3.up * hit.distance, Color.red);
+//             }
+//             else
+//             {
+//                 s.IsUnderground = false;
+//                 Debug.DrawRay(s.point.position, Vector3.up, Color.green);
+//             }
+//             collectors[i] = s;
+//         }
+//         //build mesh from edges
+//         var verts = new List<Vector3>();
+//         var tris = new List<int>();
+
+//         int triCount = 0;
+//         // this loop is about build the mesh.
+//         for (int i = 0; i < collectors.Count; i++)
+//         {
+//             var s = collectors[i];
+//             if (s.IsUnderground == true)
+//             {
+
+//                 verts.Add(s.point.position);
+
+//                 verts.Add(s.point.position + s.amount);
+
+//                 verts.Add(s.point.position + Vector3.forward);
+
+//                 verts.Add(s.point.position + s.amount + Vector3.forward);
+
+//                 tris.Add(triCount++);
+//                 // tris.Add(tris.Count - 1);
+
+//             }
+
+//         }
+
+//         Debug.Log(verts.Count + " " + tris.Count);
+//         // var mesh = new Mesh();
+//         // var mesh = sandMesh.mesh;
+//         // mesh.Clear();
+//         // mesh.vertices = verts.ToArray();
+//         // // mesh.triangles = tris.ToArray();
+//         // mesh.SetIndices(tris.ToArray(), MeshTopology.Quads, 0);
+
+//         // mesh.RecalculateBounds();
+//         // mesh.RecalculateNormals();
+//         // mesh.RecalculateTangents();
+
+
+//         // sandMesh.sharedMesh = mesh;
+//     }
+// }
+
+
+
 using UnityEngine;
+
 
 public class Shovel : MonoBehaviour
 {
-    public Transform edgeRoot;
+    public Transform edgRoot;
+    Transform[] edgePoints;
+
+    float[] sandAmount;
     public LayerMask groundMask;
-    public MeshFilter sandMesh;
-    List<Collector> collectors = new List<Collector>();
-    // Start is called before the first frame update
+
     void Start()
     {
-        for (int i = 0; i < edgeRoot.transform.childCount; i++)
-        {
-            var c = new Collector();
-            c.point = edgeRoot.GetChild(i);
-            c.amount = Vector3.zero;
-            c.IsUnderground = false;
-            collectors.Add(c);
-        }
-        var mesh = new Mesh();
-        sandMesh.mesh = mesh;
+        edgePoints = new Transform[edgRoot.transform.childCount];
+        sandAmount = new float[edgePoints.Length];
 
+        for (int i = 0; i < edgePoints.Length; i++)
+        {
+            edgePoints[i] = edgRoot.GetChild(i);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < collectors.Count; i++)
+        for (int i = 0; i < edgePoints.Length; i++)
         {
-            var s = collectors[i];
             RaycastHit hit;
-            if (Physics.Raycast(s.point.position, Vector3.up, out hit, 1, groundMask))
+            if (Physics.Raycast(edgePoints[i].position, Vector3.up, out hit, 1, groundMask))
             {
-                s.IsUnderground = true;
-                if (hit.distance > s.amount.y)
+                Debug.DrawRay(edgePoints[i].position, Vector3.up * hit.distance, Color.red);
+                if (hit.distance > sandAmount[i])
                 {
-                    s.amount.y = hit.distance;
+                    sandAmount[i] = hit.distance;
                 }
-                Debug.DrawLine(s.point.position, Vector3.up * hit.distance, Color.red);
+
             }
             else
             {
-                s.IsUnderground = false;
-                Debug.DrawRay(s.point.position, Vector3.up, Color.green);
+                Debug.DrawRay(edgePoints[i].position, Vector3.up, Color.green);
             }
-            collectors[i] = s;
         }
-
-        // var verts = new List<Vector3>();
-        // var tris = new List<int>();
-
-        // // this loop is about build the mesh.
-        // for (int i = 0; i < collectors.Count; i++)
-        // {
-        //     var s = collectors[i];
-        //     if (s.IsUnderground)
-        //     {
-
-        //         verts.Add(s.point.position);
-        //         tris.Add(tris.Count - 1);
-        //         verts.Add(s.point.position + s.amount);
-        //         tris.Add(tris.Count - 1);
-
-        //     }
-
-        // }
-        // // var mesh = new Mesh();
-        // var mesh = sandMesh.mesh;
-
-        // mesh.vertices = verts.ToArray();
-        // // mesh.triangles = tris.ToArray();
-        // mesh.SetIndices(tris.ToArray(), MeshTopology.Quads, 0);
-
-        // mesh.RecalculateBounds();
-        // mesh.RecalculateNormals();
-        // mesh.RecalculateTangents();
-
-
-        sandMesh.sharedMesh = mesh;
     }
 }
